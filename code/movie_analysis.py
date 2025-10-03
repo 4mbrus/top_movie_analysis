c3b58e7de9dab9a173c54eaaa5662d6a91933e36
Ambrus <ambrusfodor.online@gmail.com>
2025-10-04 00:51:16 +0200

    preparing data

--- code/movie_analysis.py
+++ code/movie_analysis.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+path = "C:/Users/Ambrus/OneDrive - Central European University (CEU GmbH Hungarian Branch Office)/top_movie_analysis"
 
+df = pd.read_csv(f"{path}/raw/top250movies.csv")
+
+year_list = [x for x in range(1920, 2025)]
+
+movies_per_year = df["Year"].value_counts().sort_index(ascending=False)
+movies_per_year = movies_per_year.sort_index(ascending=True)
+print(movies_per_year)
